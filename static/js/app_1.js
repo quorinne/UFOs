84f6e3866640bb62da1d6db248e3a4cd2d15c8a6
Brittanie <brittaniejones@ymail.com>
2022-04-23 12:04:24 -0700

    app.js

--- static/js/app_1.js
+++ static/js/app_1.js
@@ -28,7 +28,7 @@ function buildTable(data) {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
-  
+ 
      // Check to see if a date was entered and filter the
     // data using that date.
     if (date) {
